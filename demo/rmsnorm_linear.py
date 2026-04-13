bb041fd166cffbcd12fc18a1dc577bf010fc2156
Kevin Wang <imkevin.wang@mail.utoronto.ca>
2026-04-13 15:19:25 -0400

    update ms references in demos

--- demo/rmsnorm_linear.py
+++ demo/rmsnorm_linear.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
         print(ExperimentOutput.header())
         print(torch_output.values())
         print(cdsl_output.values())
-        print(torch_output.q50_ms / cdsl_output.q50_ms)
+        print(torch_output.ms_median / cdsl_output.ms_median)
