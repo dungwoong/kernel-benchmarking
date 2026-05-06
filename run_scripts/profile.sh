72c4a29fb89a518b22bd386ffd14abcd3ca05504
Adel Muursepp <adel.muursepp@gmail.com>
2026-05-06 16:44:29 -0400

    Add flashinfer

--- run_scripts/profile.sh
+++ run_scripts/profile.sh
@@ -1,5 +1,3 @@
-# Profiles fused workloads on some preliminary shapes
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. & pwd)"
 
 python3 $DIR/demo/gen_header.py ExperimentOutput
@@ -9,6 +7,7 @@ do
     python3 $DIR/demo/gemm.py $VAL $VAL $VAL --to_csv
     python3 $DIR/demo/lora.py $VAL $VAL $VAL 16 --to_csv
     python3 $DIR/demo/rmsnorm_linear.py $VAL $VAL $VAL --to_csv
+    python3 $DIR/demo/flashinfer_rmsnorm.py $VAL $VAL $VAL --to_csv
     python3 $DIR/demo/swiglu.py $VAL $VAL $VAL --to_csv
     sleep 2
 done
--- slurm_profile.sh
+++ slurm_profile.sh
@@ -11,4 +11,12 @@
 # e.g. sbatch --export=ALL,CONTAINER="../CuteDSL129.sif" slurm_profile.sh
 echo using container $CONTAINER
 module load apptainer
-apptainer exec --nv $CONTAINER bash -c "source ./add_path.sh && ./run_scripts/profile.sh > profile_data.csv"
+
+# Create and bind the flashinfer cache to apptainer to not run into disk quota errors
+mkdir -p $SCRATCH/flashinfer_cache
+
+# Override to the apptainer CUDA version (12.9)
+apptainer exec --nv \
+  --env CUDA_HOME=/usr/local/cuda \
+  -B $SCRATCH/flashinfer_cache:$HOME/.cache/flashinfer \
+  $CONTAINER bash -c "source ./add_path.sh && ./run_scripts/profile.sh > profile_data.csv"
