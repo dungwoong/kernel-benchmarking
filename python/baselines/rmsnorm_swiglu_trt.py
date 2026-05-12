1e5aa43a8620a669a65a10c199c7633673f08796
Adel Müürsepp <adel.muursepp@gmail.com>
2026-05-12 15:51:48 -0400

    Remove todo

--- python/baselines/rmsnorm_swiglu_trt.py
+++ python/baselines/rmsnorm_swiglu_trt.py
@@ -6,7 +6,6 @@ from baselines.rmsnorm_swiglu import swiglu_concat
 
 def _rmsnorm(x: torch.Tensor, eps: float) -> torch.Tensor:
     # aten::rms_norm has no ONNX exporter symbolic, so decompose
-    # TODO confirm TRT re-fuses this (TRT_DEBUG=1, check Normalization layer).
     x_fp32 = x.to(torch.float32)
     variance = (x_fp32 * x_fp32).mean(dim=-1, keepdim=True)
     return (x_fp32 * torch.rsqrt(variance + eps)).to(x.dtype)
