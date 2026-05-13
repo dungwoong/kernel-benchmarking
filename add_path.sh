0b84bbd7f719a2f871f20134aac2afcc9cc2cc08
Adel Müürsepp <adel.muursepp@gmail.com>
2026-05-13 11:16:53 -0400

    Venv in apptiainer

--- add_path.sh
+++ add_path.sh
@@ -1,3 +1,4 @@
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+[ -f "$REPO_ROOT/.venv/bin/activate" ] && source "$REPO_ROOT/.venv/bin/activate"
 export PYTHONPATH="$PYTHONPATH:$REPO_ROOT/python:$REPO_ROOT/third_party/fused_kernels_cutedsl_lib/python:$REPO_ROOT/third_party/cublas_gemm/python"
 export TORCH_CUDA_ARCH_LIST="9.0" # for torch extensions
