53238e89acc5da1433fb2395b9655a0d0642bbd6
Kevin Wang <wangke61@cs.toronto.edu>
2026-08-27 15:54:30 -0400

    refactor gemm_new

--- demo/new_profiling/gemm_new.py
+++ demo/new_profiling/gemm_new.py
@@ -5,8 +5,8 @@ from workload_shapes import GEMM_ARGS_NT
 from profile_utils import ProfilingJob, get_profiling_job_args
 from cdsl_fn_utils import compile_cutedsl
 
-from kernels.hel.gemm import get_kernel as get_c2_kernel
-from compiler import compile_hel
+from compiler_2.kernels.hel.gemm import get_kernel as get_c2_kernel
+from compiler_2 import compile_hel
 from helion_utils.kernel_runner import Matmul
 
 """
